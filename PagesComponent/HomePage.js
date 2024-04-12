328c3676d790ffe06cf69f1a74cdf0c4a24c16ac
Namez <you@exemple.com>
2024-04-12 10:14:47 +0200

    2e commit 10h

--- PagesComponent/HomePage.js
+++ PagesComponent/HomePage.js
@@ -1,15 +1,37 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,ImageBackground } from 'react-native';
+import bg from '../assets/bg.jpg'
 
 export default function HomePage() {
     return (
         <>
-        <Text variant="displayLarge">Display Large</Text>
+        <View style={styles.container}>
+        <ImageBackground source={bg} resizeMode="cover" style={styles.image}>
+                <Text variant="displayLarge" style={styles.textHeader}>A table !</Text>
+        </ImageBackground>
+        </View>
+
+
+        
         </>
     );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+    flex: 1,
+    display : 'flex',
+    alignItems: 'center',
+    },
     
+    textHeader : {
+        fontSize : 30,
+        color : 'white',
+        left : '40%',
+        top : '20%',
+    },
+    image : {
+        width : '100%',
+        height : '50%',
+    },
 });
