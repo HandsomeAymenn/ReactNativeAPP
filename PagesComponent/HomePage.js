69f2bbba01b8e4008b149d53746e8ce68724e96a
Namez <you@exemple.com>
2024-04-12 09:38:37 +0200

    ga

---
+++ PagesComponent/HomePage.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomePage() {
+    return (
+        <>
+        <Text variant="displayLarge">Display Large</Text>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+
+    
+});
